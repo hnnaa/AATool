8ee888847d7fdc92df32239627e81dca9c57b0bb
hnnaa <hnnaa@sina.com>
2020-06-29 09:54:19 +0800

    log

---
+++ UtilsLib/IO/Log/ILog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UtilsLib.IO.Log
+{
+    public interface ILog
+    {
+        void WriteLogMessage(string msg);
+
+        void WriteErrorLogMessage(string msg);
+    }
+}
